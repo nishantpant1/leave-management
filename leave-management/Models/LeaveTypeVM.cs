d90f1bb00c8307b63c15ea3d91b79aee68883d4f
nishantpant1 <nishantpant1@gmail.com>
2020-09-19 20:10:57 +0530

    Project_1st_Commit

---
+++ leave-management/Models/LeaveTypeVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class CreateLeaveTypeVM
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+
+    public class DetailLeaveTypeVM
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
